0a21fdffc51304f772756af6c65cb27313f34600
Aryan Gupta <ary.guptag@gmail.com>
2026-05-07 08:31:09 -0400

    feat: add prettier + run prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [],
-	server: {
-		port: 3000,
-	},
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  integrations: [],
+  server: {
+    port: 3000,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
